fd211ac8ca51e553a859c91b5c486eda2abe8ba6
c0l0l0s <hub4realtil@gmail.com>
2022-04-16 04:33:18 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/ahomasuo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ahomasuo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/order_room.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//list of all orders
+//streamview
+class OrderRoom extends StatefulWidget {
+  const OrderRoom({Key? key}) : super(key: key);
+
+  @override
+  State<OrderRoom> createState() => _OrderRoomState();
+}
+
+class _OrderRoomState extends State<OrderRoom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
