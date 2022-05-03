1fd7f6bbeefdeaf54df95461a87cee8fe55b0927
c0l0l0s <hub4realtil@gmail.com>
2022-05-03 10:31:29 +0000

    eid fitr 1st commit

--- lib/views/providerpractise/providertrial.dart
+++ lib/views/providerpractise/providertrial.dart
@@ -8,19 +8,33 @@ class ProviderTrial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    var message = Provider.of<Counter>(context).getCounter;
+    var message = Provider.of<Counter>(context, listen: true).getCounter;
+    var increase = Provider.of<Counter>(context, listen: true).getIncreaseCount;
+    var decrease = Provider.of<Counter>(context, listen: true).getDecreaseCount;
 
     return Container(
       child: Stack(
         children: [
-          Center(
-            child: Column(
-              children: [
-                Text('you have counted this number of times in total'),
-                Text('$message',
-                    style: Theme.of(context).textTheme.headline4)
-              ]
-            ),
+          Column(
+            children: [
+              Center(child: Text('you have counted a total number of ')),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('$message',
+                        style: Theme.of(context).textTheme.headline4
+                    ),
+                    Text('times')
+                  ],
+                ),
+              ),
+
+              Spacer(),
+              Text('you have tapped decrease button ${decrease.toString()} times'),
+              Spacer(),
+              Text('you have tapped increase button ${increase.toString()} times')
+            ]
           ),
 
           Positioned(
@@ -35,7 +49,7 @@ class ProviderTrial extends StatelessWidget {
           ),
 
           Positioned(
-              right: 40,
+              left: 40,
               top: 40,
               child: FloatingActionButton(
                 child: Icon(Icons.minimize_rounded),
@@ -52,17 +66,33 @@ class ProviderTrial extends StatelessWidget {
 
 class Counter extends ChangeNotifier{
   var _count = 0;
+  var _incrementCount = 0;
+  var _decrementCount = 0;
 
   int get getCounter {
     return _count;
   }
 
+  int get getIncreaseCount{
+    return _incrementCount;
+  }
+
+  int get getDecreaseCount {
+    return _decrementCount;
+  }
+
   void incrementCounter() {
     _count += 1;
+    _incrementCount += 1;
+    notifyListeners();
+    //print(_count);
   }
 
   void decreamentCounter(){
     _count -= 1;
+    _decrementCount += 1;
+    notifyListeners();
+    //print(_count);
   }
 }
 
